3decd41ba9d6ecc3a3deb42a08c307fb7f6d4832
Marcin Wierzbiński <marcin119a@gmail.com>
2020-08-12 15:31:10 +0200

    create_dataset function

--- utilities.py
+++ utilities.py
@@ -22,3 +22,11 @@ def preprocessing(dataset):
   dataset = scaler(dataset)
   
   return dataset
+
+def create_dataset(dataset, look_back=1):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-look_back-1):
+		a = dataset[i:(i+look_back), 0]
+		dataX.append(a)
+		dataY.append(dataset[i + look_back, 0])
+	return np.array(dataX), np.array(dataY)
