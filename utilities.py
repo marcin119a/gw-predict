e0ea369fdcfa14f4ec4c2bbd5c10297604beab17
Marcin Wierzbiński <marcin119a@gmail.com>
2020-08-12 14:44:09 +0200

    update preprocessing and split_dataset

--- utilities.py
+++ utilities.py
@@ -5,4 +5,18 @@ import numpy as np
 def scaler(dataset):
   scaler = MinMaxScaler(feature_range=(0, 1))
   dataset = scaler.fit_transform(dataset.reshape(-1,1))
-return dataset
+  
+  return dataset
+
+
+def split_dataset(dataset):
+  train_size = int(len(dataset) * 0.67)
+  test_size = len(dataset) - train_size
+  train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+  
+  return train, test
+
+
+def preprocessing(dataset):
+  dataset = np.array([i for i in dataset])
+  dataset = scaler(dataset)
