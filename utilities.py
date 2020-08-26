22e30a6a5122fd8d30ea4e59787f410d96c07f64
Marcin Wierzbiński <marcin119a@gmail.com>
2020-08-26 16:33:34 +0200

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -22,3 +22,13 @@ def create_dataset(dataset, look_back=1):
 		dataX.append(a)
 		dataY.append(dataset[i + look_back, 0])
 	return np.array(dataX), np.array(dataY)
+
+def split_sequences_wave(sequences, params):
+  X, y = list(), list()
+  for seq in sequences:
+    X.append([seq])
+  
+  for x in params:
+  	y.append(params)
+  
+  return array(X), y
