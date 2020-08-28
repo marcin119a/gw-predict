2db54ddb9d40023424cc395cb0e207b5b2beefa9
Marcin Wierzbiński <marcin119a@gmail.com>
2020-08-28 22:43:09 +0200

    Update generator_dataset.py

--- generator_dataset.py
+++ generator_dataset.py
@@ -4,13 +4,13 @@ from hyperopt import hp
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 
-def random_dataset(m1, m2):
+def random_dataset(m1, m2, n_steps, iteraction):
   apx = 'SEOBNRv4'
   
 
   X = []
   y = []
-  for _ in range(100):
+  for _ in range(iteraction):
       mass1 = np.random.uniform(m1,m2)
       mass2 = np.random.uniform(m1,m2)
       scaler = MinMaxScaler(feature_range=(0, 1))
@@ -28,7 +28,7 @@ def random_dataset(m1, m2):
       }
 
       signal_h1, signal_l1, signal_v1 = generate_wave(params)
-      signal_h1.resize(1400)
+      signal_h1.resize(n_steps)
       x1 = np.array([step for step in signal_h1])
       x1 = scaler.fit_transform(x1.reshape(-1,1))
 
