3fd73c88e92a6cd1a6ecd666c5a6dda4df8bcbd3
Marcin Wierzbiński <marcin119a@gmail.com>
2020-08-28 20:57:32 +0200

    Update generator_dataset.py

--- generator_dataset.py
+++ generator_dataset.py
@@ -1,6 +1,8 @@
+import tensorflow as tf
 from generator_waveform import generate_wave
 from hyperopt import hp
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
 def random_dataset(m1, m2):
   apx = 'SEOBNRv4'
@@ -11,7 +13,8 @@ def random_dataset(m1, m2):
   for _ in range(100):
       mass1 = np.random.uniform(m1,m2)
       mass2 = np.random.uniform(m1,m2)
-
+      scaler = MinMaxScaler(feature_range=(0, 1))
+      
       params = {
           'approximant': apx,
           'mass1': mass1,
@@ -27,6 +30,7 @@ def random_dataset(m1, m2):
       signal_h1, signal_l1, signal_v1 = generate_wave(params)
       signal_h1.resize(1400)
       x1 = np.array([step for step in signal_h1])
+      x1 = scaler.fit_transform(x1.reshape(-1,1))
 
       X.append(x1)
       y.append(np.array([mass1, mass2]))
