510c12b2b4c36b1f6e4d48ba832c74ff73b0500d
Marcin Wierzbiński <marcin119a@gmail.com>
2020-09-01 11:46:46 +0200

    normalize feature, problem with nan loss

--- generator_dataset.py
+++ generator_dataset.py
@@ -1,8 +1,9 @@
+from generator_waveform import generate_wave
+from generator_dataset import random_dataset
 import tensorflow as tf
 from generator_waveform import generate_wave
 from hyperopt import hp
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
 
 def random_dataset(m1, m2, n_steps, iteraction):
   apx = 'SEOBNRv4'
@@ -11,9 +12,8 @@ def random_dataset(m1, m2, n_steps, iteraction):
   X = []
   y = []
   for _ in range(iteraction):
-      mass1 = np.random.uniform(m1,m2)
-      mass2 = np.random.uniform(m1,m2)
-      scaler = MinMaxScaler(feature_range=(0, 1))
+      mass1 = np.random.uniform(m1, m2)
+      mass2 = np.random.uniform(m1, m2)
       
       params = {
           'approximant': apx,
@@ -30,10 +30,13 @@ def random_dataset(m1, m2, n_steps, iteraction):
       signal_h1, signal_l1, signal_v1 = generate_wave(params)
       signal_h1.resize(n_steps)
       x1 = np.array([step for step in signal_h1])
-      x1 = scaler.fit_transform(x1.reshape(-1,1))
-
+      x1 = tf.keras.utils.normalize(x1, axis = -1)
+      
+      y1 = np.array([mass1, mass2])
+      y1 = tf.keras.utils.normalize(y1, axis = -1)
+      
       X.append(x1)
-      y.append(np.array([mass1, mass2]))
+      y.append(y1)
 
 
   return np.array(X), np.array(y)
