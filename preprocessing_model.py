a06cbdcc1d2c96153ec7065bcc41f986c9fbc372
Marcin Wierzbiński <marcin119a@gmail.com>
2020-09-16 16:58:49 +0200

    Update preprocessing_model.py

--- preprocessing_model.py
+++ preprocessing_model.py
@@ -13,7 +13,10 @@ X_test = array_hkl.get('xtest')
 y_train = array_hkl.get('ytrain')
 y_test = array_hkl.get('ytest')
 
-scaler = MinMaxScaler()
-# transform data
-scaled = scaler.fit_transform(X)
-print(scaled)
+from sklearn.preprocessing import MinMaxScaler
+
+scalers = {}
+for i in range(X_train.shape[1]):
+    scalers[i] = MinMaxScaler()
+    X_train[:, i, :] = scalers[i].fit_transform(X_train[:, i, :]) 
+
