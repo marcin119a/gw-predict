0e90b4b4a4617a8ad4a101fd48d0d45eac762a02
Marcin Wierzbiński <mwierzbinski@camk.edu.pl>
2021-06-09 12:33:08 +0200

    model cnn fix problem with GPU

--- model_cnn.py
+++ model_cnn.py
@@ -5,21 +5,21 @@ from keras.layers.normalization import BatchNormalization
 
 #(batch_size, timesteps, units)
 #(?, 1024, 1) 
-def create_model(activation='tanh', lr=1e-3, reg=0.0, dropout=0.0, num_neurons=200, batch_normalization=False, n_steps_in=1):
+def create_model(activation='tanh', lr=1e-3, reg=0.0, dropout=0.5, num_neurons=200, batch_normalization=False, n_steps_in=1):
   n_steps_out = 1200
   model_m = Sequential()
   model_m.add(Conv1D(filters=64, kernel_size=16, strides=4, activation='relu'))  
-  model_m.add(Dropout(0.5)) 
+  model_m.add(Dropout(dropout)) 
   model_m.add(MaxPooling1D(4))
 
   model_m.add(Conv1D(filters=32, kernel_size=8, strides=2, activation='relu'))
-  model_m.add(Dropout(0.5))
+  model_m.add(Dropout(dropout))
   model_m.add(MaxPooling1D(2))
 
   model_m.add(GlobalAveragePooling1D())
   model_m.add(Dropout(0.1))
 
-  model_m.add(Dense(units=n_steps_out))
+  model_m.add(Dense(units=1))
   model_m.compile(optimizer='adam', loss='mse')
 
   return model_m
